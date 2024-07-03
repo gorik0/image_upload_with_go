a49dd072bcb1c6aba269705bcfb55ea0aef854ad
gorik0 <delavarus@gmail.com>
2024-07-03 21:55:16 +0300

    working_handler

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	router := gin.Default()
+	router.POST("/image", imageHandler)
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		panic(err)
+	}
+
+}
+
+func imageHandler(c *gin.Context) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		log.Printf("Error getting file: %v", err)
+	}
+	println(file.Filename)
+}
